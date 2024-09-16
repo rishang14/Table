6a3e61e6b5c3d85e6648dfc1918bf8523c93a0a9
Rishang <rishangkumar9693@gmail.com>
2024-09-16 16:38:03 +0530

    edit func is added in table

--- src/Comp/Table.jsx
+++ src/Comp/Table.jsx
@@ -1,29 +1,59 @@
 import React, { useEffect, useState } from "react";
 import InputComp from "./FormInput";
-import { data } from "autoprefixer";
 
 const Table = () => {
   const [tableData, setTableData] = useState([]);
+  const [editableRow, setEditableRow] = useState("");
   let keys;
   if (tableData.length > 0) {
     keys = Object.keys(tableData[0]).filter((key) => key !== "id");
     keys.push("Action");
-  } 
+  }
 
-  //HELPER FUNCTION 
-  function filterLogic(item,id){
-    return item.id !== id
+  //HELPER FUNCTION
+  function filterLogic(item, id) {
+    return item.id !== id;
   }
 
   const handleDelete = (id) => {
     //filter based on id
-    const newdata = tableData.filter(item => filterLogic(item,id)); 
-    //get data from localestorage 
-    const StorageData=JSON.parse(localStorage.getItem("usersDetails")) 
-    const filteredData=StorageData.filter(item => filterLogic(item,id))
+    const newdata = tableData.filter((item) => filterLogic(item, id));
+    //get data from localestorage
+    const StorageData = JSON.parse(localStorage.getItem("usersDetails"));
+    const filteredData = StorageData.filter((item) => filterLogic(item, id));
     //update localstorage and table with new data
-     localStorage.setItem("usersDetails", JSON.stringify(filteredData));
-     setTableData(newdata);
+    localStorage.setItem("usersDetails", JSON.stringify(filteredData));
+    setTableData(newdata);
+  };
+
+  const handleEdit = (id) => {
+    setEditableRow(id);
+  };
+
+  const handleEditChange = (e, id) => {
+    //filtering in tabledata
+    const newdata = tableData.map((item) => {
+      //if it matches
+      if (item.id === id) {
+        //make changes
+        return { ...item, [e.target.name]: e.target.value };
+      }
+      //return previous ones
+      return item;
+    });
+ 
+    //set in tabledate and localestroage
+    setTableData(newdata);
+    localStorage.setItem("usersDetails", JSON.stringify(newdata));
+  };
+
+  const handleSave = (id) => {
+    //fetch from localeStorage
+    const data = JSON.parse(localStorage.getItem("usersDetails"));
+    //set in table
+    setTableData(data);
+    //make editable null so it not show
+    setEditableRow("");
   };
 
   return (
@@ -67,15 +97,36 @@ const Table = () => {
                         >
                           {key == "Action" ? (
                             <>
-                              <button className="p-2 border border-b-black rounded-lg gap-2 mr-[10px] ">
-                                Edit
-                              </button>
+                              {editableRow == data.id ? (
+                                <button
+                                  className="p-2 border border-b-black rounded-lg gap-2 mr-[10px] "
+                                  onClick={() => handleSave(data.id)}
+                                >
+                                  Save
+                                </button>
+                              ) : (
+                                <button
+                                  className="p-2 border border-b-black rounded-lg gap-2 mr-[10px] "
+                                  onClick={() => handleEdit(data.id)}
+                                >
+                                  Edit
+                                </button>
+                              )}
                               <button
                                 className="p-2 border border-b-black rounded-lg gap-2 "
                                 onClick={() => handleDelete(data.id)}
                               >
                                 Delete
                               </button>
+                            </>
+                          ) : editableRow == data.id ? (
+                            <>
+                              <input 
+                              name={key}
+                                className="p-2  w-full"
+                                value={data[key]}
+                                onChange={(e) => handleEditChange(e, data.id)}
+                              />
                             </>
                           ) : (
                             data[key]
